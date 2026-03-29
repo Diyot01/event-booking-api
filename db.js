709ef2a09e27b1c39a1bdd7e527f9fc8c99794ba
Diyot01 <ayushpersonal2005@gmail.com>
2026-03-30 05:09:30 +0530

    db.js updated error fix by url env

--- db.js
+++ db.js
@@ -1,26 +1,14 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT,
-    ssl: {
-        rejectUnauthorized: false
-    },
-    waitForConnections: true,
-    connectionLimit: 10
-});
+const connection = mysql.createConnection(process.env.MYSQL_URL);
 
-connection.getConnection((err, conn) => {
+connection.connect((err) => {
     if (err) {
         console.log("DB ERROR:", err);
         console.log("Database connection failed");
         return;
     }
     console.log("Connected to MySQL");
-    conn.release();
 });
 
 module.exports = connection;
