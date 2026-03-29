cda8d01061978e1aa24424d3065b020ad1eea1ae
Diyot01 <ayushpersonal2005@gmail.com>
2026-03-30 04:59:59 +0530

    db.js updated error fix

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
@@ -8,16 +8,19 @@ const connection = mysql.createConnection({
     port: process.env.MYSQLPORT,
     ssl: {
         rejectUnauthorized: false
-    }
+    },
+    waitForConnections: true,
+    connectionLimit: 10
 });
 
-connection.connect((err) => {
+connection.getConnection((err, conn) => {
     if (err) {
         console.log("DB ERROR:", err);
         console.log("Database connection failed");
         return;
     }
     console.log("Connected to MySQL");
+    conn.release();
 });
 
 module.exports = connection;
